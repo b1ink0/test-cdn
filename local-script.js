66f7b6492ddb605ce04472d0e97f51906db09966
Aditya Dhade <76063440+b1ink0@users.noreply.github.com>
2023-06-08 16:32:22 +0530

    n

--- local-script.js
+++ local-script.js
@@ -1,5 +1,7 @@
 // react-widget-script.js version: 1.1
-let BACKEND_URL = "https://geometry-rca-zoo-british.trycloudflare.com";
+
+
+let BACKEND_URL = "https://mad-offshore-interested-connections.trycloudflare.com";
 let shopDomain_rs;
 let customerId_rs;
 if (__st.cid !== undefined) {
